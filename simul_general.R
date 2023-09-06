f3de1035de1fb0b9c8ef44af1a7a08a678ad891b
Alberto Bordino <54471782+abordino@users.noreply.github.com>
2023-09-06 18:47:54 +0100

    Update simul_general.R

--- simul_general.R
+++ simul_general.R
@@ -4,134 +4,8 @@ library(npmr)
 library(matrixcalc)
 library(pracma)
 
-############## INSPECTING THE PATTERNS #############
-
-#### PATTERNS SPECIFIED BY THE USER
-patterns = list(c(1,2,3),c(1,2,4))
-card_patterns = length(patterns)
-
-d = 0 # computing d
-for (k in card_patterns){
-  S = patterns[[k]]
-  if (max(S) > d){
-    d = max(S)
-  }
-}
-
-# ###### RANDOM PATTERNS
-# d = 1
-# while(d==1){
-#   d = rpois(1, lambda = 5)
-# }
-# v = 1:d
-# patterns = list()
-# 
-# for (i in 1:rpois(1, lambda = 7)){
-#   tmp = rbinom(d,1,1/2)
-#   while (sum(tmp)==0){
-#     tmp = rbinom(d,1,1/2)
-#   }
-#   tmp = as.logical(tmp)
-#   patterns[[i]] = v[tmp]
-# }
-# 
-# # remove duplicates
-# patterns = unique(patterns)
-# card_patterns = length(patterns)
-
-###### print patterns
-print(paste("The dimensionality of the problem is",d))
-print(paste("There are", card_patterns, "patterns:"))
-for (S in patterns){
-  print(S)
-}
-
-########## GENERATE SIGMA_S ############
-# R > 0
-SigmaS=list() 
-for(j in 1:card_patterns){
-  card_S = length(patterns[[j]])
-  A = matrix(runif((card_S)^2)*2-1, ncol=card_S) 
-  tmp_0 = t(A) %*% A
-  SigmaS[[j]]=as.matrix(cov2cor(tmp_0))
-}
-
-# # R = 0
-# SigmaS=list()
-# A = matrix(runif(d^2)*2-1, ncol=d)
-# Sigma = cov2cor(t(A) %*% A)
-# for(j in 1:card_patterns){
-#   SigmaS[[j]]=as.matrix(Sigma[patterns[[j]],patterns[[j]]])
-# }
-
-########## WRITING THE SDP PROBLEM ##########################
-# Matrix giving objective function (1/d)*tr(Sigma)
-C = c(list((1/d)*diag(d)))
-for(j in 1:card_patterns){
-  card_S = length(patterns[[j]])
-  C[[1+j]] = matrix(0, card_S, card_S)
-}
-
-# number of constraints
-m = d-1
-for(j in 1:card_patterns){
-  card_S = length(patterns[[j]])
-  m = m + card_S*(card_S+1)/2
-}
-
-b=rep(0,m)
-
-A=rep(list(0),m)
-Ablank=c(list(matrix(0,d,d)))
-for(j in 1:card_patterns){
-  card_S = length(patterns[[j]])
-  Ablank[[1+j]] = matrix(0, card_S, card_S)
-}
-
-
-ind = 0
-for(k in 1:card_patterns){
-  card_S = length(patterns[[k]])
-  for (i in 1:card_S){
-    for (j in i:card_S){
-      b[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i] = SigmaS[[k]][i,j]
-      
-      e_i = rep(0, card_S); e_i[i]=1
-      e_j = rep(0, card_S); e_j[j]=1
-      A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]] = Ablank
-      A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]][[k+1]] = e_i%*%t(e_j)/2 + e_j%*%t(e_i)/2
-      
-      tmp_1 = e_i%*%t(e_j)/2 +  e_j%*%t(e_i)/2
-      tmp_2 = matrix(0,d,d)
-      tmp_2[patterns[[k]],patterns[[k]]] = tmp_1
-      A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]][[1]] = tmp_2
-    }
-  }
-  ind = ind + card_S*(card_S+1)/2
-}
-
-for (i in (d-2):0){
-  A[[m-i]] = Ablank
-  A[[m-i]][[1]][1,1]=1
-  A[[m-i]][[1]][2+i,2+i]=-1
-}
-
-sizes = c(d)
-for (S in patterns){
-  card_S = length(S)
-  sizes = c(sizes, card_S)
-}
-K = list(type=rep("s",card_patterns+1),size=sizes) # Variables are the PSD matrices Sigma,Z_{1,2},...,Z_{d,1}
-
-SDP=csdp(C, A, b, K) #Running the function
-R = 1-SDP$pobj
-
-
 
 ############ FUNCTION ###############
-#----------------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------------
 computeR = function(patterns=list(), SigmaS=list()) {
   library(Rcsdp)
   library(Matrix)
@@ -358,21 +232,34 @@ result = computeR(patterns = list(c(2,3,4,5),c(1,3,4,5),c(1,2,4,5),c(1,2,3,5),c(
 AAA = result$SigmaS_prime
 computeR(patterns = list(c(2,3,4,5),c(1,3,4,5),c(1,2,4,5),c(1,2,3,5),c(1,2,3,4)), SigmaS = AAA)$R
 
+############# test on 3-cycle ####################
+R_rho = c()
+d = 3
+SigmaS=list() #Random 2x2 correlation matrices (necessarily consistent)
+for(j in 1:d){
+  x=runif(2,min=-1,max=1); y=runif(2,min=-1,max=1); SigmaS[[j]]=cov2cor(x%*%t(x) + y%*%t(y))
+}
+
+SigmaS[[1]][1,2] = cos(0)
+SigmaS[[1]][2,1] = cos(0)
+SigmaS[[2]][1,2] = cos(0)
+SigmaS[[2]][2,1] = cos(0)
+
+for (rho in seq(0, pi, length.out = 1000)){
+SigmaS[[3]][1,2] = cos(rho)
+SigmaS[[3]][2,1] = cos(rho)
+
+result = computeR(list(c(1,2),c(2,3), c(1,3)), SigmaS = SigmaS)
+R_rho = c(R_rho, result$R)
+}
+
+plot(seq(0, pi, length.out = 1000), R_rho, col="red", pch=19, cex=0.1)
 
 ################## 1-Schatten + maximum modulus LOWER and UPPER BOUND. ########
 
-n_k = c()
-n_M = c()
+m_k_plus_s_k = c()
 R_k = c()
-l0 = c()
-l1 = c()
-l2 =c()
-l4 =c()
-l10 =c()
-linf = c()
-
-
-d=6
+d=4
 
 for (kkk in 1:5000){
   SigmaS=list() 
@@ -385,61 +272,16 @@ for (kkk in 1:5000){
     print("NOT PSD")
     next
   }
-  result = computeR(list(c(1,2,3,4,5),c(1,2,3,4,6)), SigmaS = SigmaS)
+  result = computeR(list(c(1,2,3),c(1,2,4)), SigmaS = SigmaS)
   
   R_k = c(R_k, result$R)
+  tmp = norm(SigmaS[[1]][c(-(d-1)),c(-(d-1))]-SigmaS[[2]][c(-(d-1)),c(-(d-1))], type = "M")/2
+  m_k_plus_s_k = c(m_k_plus_s_k, tmp)
   
-  n_k = c(n_k, nuclear(SigmaS[[1]][c(-(d-1)),c(-(d-1))]-SigmaS[[2]][c(-(d-1)),c(-(d-1))])/(2*(d-2)))
-  n_M = c(n_M, norm((SigmaS[[1]][c(-(d-1)),c(-(d-1))]-SigmaS[[2]][c(-(d-1)),c(-(d-1))])/2,
-          type = c("M")))
-  l0 = c(l0, Norm(as.vector(SigmaS[[1]][c(-(d-1)),c(-(d-1))]-SigmaS[[2]][c(-(d-1)),c(-(d-1))]), p = 0))
-  l1 = c(l1, Norm(as.vector(SigmaS[[1]][c(-(d-1)),c(-(d-1))]-SigmaS[[2]][c(-(d-1)),c(-(d-1))]), p = 1))
-  l2 = c(l2, Norm(as.vector(SigmaS[[1]][c(-(d-1)),c(-(d-1))]-SigmaS[[2]][c(-(d-1)),c(-(d-1))]), p = 2))
-  l4 = c(l4, Norm(as.vector(SigmaS[[1]][c(-(d-1)),c(-(d-1))]-SigmaS[[2]][c(-(d-1)),c(-(d-1))]), p = 4))
-  l10 = c(l10, Norm(as.vector(SigmaS[[1]][c(-(d-1)),c(-(d-1))]-SigmaS[[2]][c(-(d-1)),c(-(d-1))]), p = 10))
-  linf = c(linf, Norm(as.vector(SigmaS[[1]][c(-(d-1)),c(-(d-1))]-SigmaS[[2]][c(-(d-1)),c(-(d-1))]), p = Inf))
 }
 
-plot(n_k, R_k, col="red", pch=19, cex=0.02, main="d = 6, Nuclear/2(d-2) in red, maximum modulus/2 in blue")
-lines(n_M, R_k, col="blue", pch=19, cex=0.02, main="d = 6, M", type="p")
+plot(m_k_plus_s_k, R_k, col="red", pch=19, cex=0.1, main="d = 4, |x-y|/2",xlim=c(0,1))
 curve(x^1, add=T)
-plot(l1,R_k, col="red", pch=19, cex=0.1, main="d = 6, l1")
-plot(l2,R_k, col="red", pch=19, cex=0.1, main="d = 6, l2")
-plot(l4,R_k, col="red", pch=19, cex=0.1, main="d = 6, l4")
-plot(l10,R_k, col="red", pch=19, cex=0.1, main="d = 6, l10")
-plot(linf,R_k, col="red", pch=19, cex=0.1, main="d = 6, linf")
 
-# curve((d-2)*x, add=T)
-
-
-
-#################### FINDING THE OPTIMAL XS and Sigma
-d=6
-SigmaS=list() 
-
-  for(j in 1:2){
-    A = matrix(runif((d-1)^2)*2-1, ncol=d-1) 
-    tmp_0 = t(A) %*% A
-    SigmaS[[j]]=round(as.matrix(cov2cor(tmp_0)),10)
-  }
-  if(is.positive.definite(SigmaS[[1]])==F || is.positive.definite(SigmaS[[2]])==F){
-    print("NOT PSD")
-    next
-  }
-
-result = computeR(list(c(1,2,3,4,5),c(1,2,3,4,6)), SigmaS = SigmaS)
-result$Sigma/(1-result$R)
-
-
-
-
-
-
-
-
-
-
-
-
-
+##### EVEN FOR d=4, WHERE R=0 IFF |x-y|=0, THE LOWER BOUND SEEMS TO BE LOOSE. WHAT ARE WE MISSING HERE?
 
