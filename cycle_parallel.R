9fcae126c15598f7166933eebb5f3d0d500b67ba
Alberto Bordino <54471782+abordino@users.noreply.github.com>
2023-11-21 14:50:05 +0000

    Update cycle_parallel.R

--- cycle_parallel.R
+++ cycle_parallel.R
@@ -1,11 +1,20 @@
 library(missMethods)
 library(MASS)
 library(norm)
-library(simsalapar)
+library(parallel)
+library(copula)
+library(missMethods)
+library(misty)
+library(Rcsdp)
+library(Matrix)
+library(npmr)
+library(matrixcalc)
+library(pracma)
+library(foreach)
+library(doSNOW)
+library(doParallel)
 
 computeR = function(patterns=list(), SigmaS=list()) {
-  library(Rcsdp)
-  library(Matrix)
   
   #----------------------------------------------------------------------------------------
   ##### DEFINE PATTERN IF NOT SPECIFIED
@@ -407,63 +416,109 @@ MCAR_corr_test = function(X, alpha, B){
   return(decision)
 }
 
-MC_rep = function(i){
-  
-  #### generate dataset from patter S = {{1,2},{2,3},{1,3}}
-  X1 = mvrnorm(n, c(0,0), SigmaS[[1]])
-  X2 = mvrnorm(n, c(0,0), SigmaS[[2]])
-  X3 = mvrnorm(n, c(0,0), SigmaS[[3]])
-  
-  columns = c("X1","X2","X3") 
-  X = data.frame(matrix(nrow = 3*n, ncol = length(columns)))
-  X[1:n, c("X1", "X2")] = X1
-  X[(n+1):(2*n), c("X2", "X3")] = X2
-  X[(2*n+1):(3*n), c("X1", "X3")] = X3
-  X = as.matrix(X)
-  
-  result_vect = c(little_test(X, alpha),
-                  little_test(X, alpha, "cov"),
-                  MCAR_corr_test(X, alpha, B = 99))
-  return(result_vect)
-}
-
-varList = varlist(
-   n.sim = list(type = "N", expr = quote(N[sim]), value = 5000),
-   n = list(type = "grid", value = c(200, 400, 1000)))
-toLatex(varList, label = "tab:var", caption = "Variables which determine our simulation study.")
-pGrid = mkGrid(varList)
-
 ######### 3-cycle: setting 1 ############
 alpha = 0.05
 n = 200
 M = 50
 t3 = pi/6
 t2 = pi/4
+n_cores = detectCores()-1
 
 little_power = c()
 little_power_cov = c()
-our_power = c()
 R = c()
 
-for(t1 in seq(t2-t3, pi - t3, length.out = 3)){
-  
-  #### POPULATION LEVEL ######
-  SigmaS=list() #Random 2x2 correlation matrices (necessarily consistent)
-  for(j in 1:3){
-    x=runif(2,min=-1,max=1); y=runif(2,min=-1,max=1); SigmaS[[j]]=cov2cor(x%*%t(x) + y%*%t(y))
-  }
-  
-  SigmaS[[1]][1,2] = cos(t1)
-  SigmaS[[1]][2,1] = cos(t1)
-  SigmaS[[2]][1,2] = cos(t2)
-  SigmaS[[2]][2,1] = cos(t2)
-  SigmaS[[3]][1,2] = cos(t3)
-  SigmaS[[3]][2,1] = cos(t3)
-  
-  R = c(R, computeR(list(c(1,2),c(2,3), c(1,3)), SigmaS)$R)
-  
-  options(mc.cores=2)
-  res = doClusterApply(1:M, doOne=MC_rep, monitor=TRUE)
-
+bootstrap_test_t1 = function(t1){
+    SigmaS=list() #Random 2x2 correlation matrices (necessarily consistent)
+    for(j in 1:3){
+      x=runif(2,min=-1,max=1); y=runif(2,min=-1,max=1); SigmaS[[j]]=cov2cor(x%*%t(x) + y%*%t(y))
+    }
+    
+    SigmaS[[1]][1,2] = cos(t1)
+    SigmaS[[1]][2,1] = cos(t1)
+    SigmaS[[2]][1,2] = cos(t2)
+    SigmaS[[2]][2,1] = cos(t2)
+    SigmaS[[3]][1,2] = cos(t3)
+    SigmaS[[3]][2,1] = cos(t3)
+    
+    ###### SAMPLE LEVEL, REPEATING THE TEST M TIMES #######
+    our_decisions = c()
+    for (i in 1:M){
+      
+      #### generate dataset from patter S = {{1,2},{2,3},{1,3}}
+      X1 = mvrnorm(n, c(0,0), SigmaS[[1]])
+      X2 = mvrnorm(n, c(0,0), SigmaS[[2]])
+      X3 = mvrnorm(n, c(0,0), SigmaS[[3]])
+      
+      columns = c("X1","X2","X3") 
+      X = data.frame(matrix(nrow = 3*n, ncol = 3))
+      X[1:n, c("X1", "X2")] = X1
+      X[(n+1):(2*n), c("X2", "X3")] = X2
+      X[(2*n+1):(3*n), c("X1", "X3")] = X3
+      X = as.matrix(X)
+      
+      ### run our tests
+      our_decisions = c(our_decisions, MCAR_corr_test(X, alpha, B = 99))
+    }
+    
+    res = mean(our_decisions)
+    return(res)
 }
+
+our_power = mclapply(seq(t2-t3, pi - t3, length.out = n_cores), bootstrap_test_t1, mc.cores = n_cores)
+
+##### easy part
+for(t1 in seq(t2-t3, pi - t3, length.out = n_cores)){
+    
+    #### POPULATION LEVEL ######
+    SigmaS=list() #Random 2x2 correlation matrices (necessarily consistent)
+    for(j in 1:3){
+      x=runif(2,min=-1,max=1); y=runif(2,min=-1,max=1); SigmaS[[j]]=cov2cor(x%*%t(x) + y%*%t(y))
+    }
+    
+    SigmaS[[1]][1,2] = cos(t1)
+    SigmaS[[1]][2,1] = cos(t1)
+    SigmaS[[2]][1,2] = cos(t2)
+    SigmaS[[2]][2,1] = cos(t2)
+    SigmaS[[3]][1,2] = cos(t3)
+    SigmaS[[3]][2,1] = cos(t3)
+    
+    R = c(R, computeR(list(c(1,2),c(2,3), c(1,3)), SigmaS)$R)
+    
+    
+    ###### SAMPLE LEVEL, REPEATING THE TEST M TIMES #######
+    little_decisions = c()
+    little_decisions_cov = c()
+    for (i in 1:M){
+      
+      #### generate dataset from patter S = {{1,2},{2,3},{1,3}}
+      X1 = mvrnorm(n, c(0,0), SigmaS[[1]])
+      X2 = mvrnorm(n, c(0,0), SigmaS[[2]])
+      X3 = mvrnorm(n, c(0,0), SigmaS[[3]])
+      
+      columns = c("X1","X2","X3") 
+      X = data.frame(matrix(nrow = 3*n, ncol = 3))
+      X[1:n, c("X1", "X2")] = X1
+      X[(n+1):(2*n), c("X2", "X3")] = X2
+      X[(2*n+1):(3*n), c("X1", "X3")] = X3
+      X = as.matrix(X)
+      
+      ### run little's test
+      little_decisions = c(little_decisions, little_test(X, alpha))
+      little_decisions_cov = c(little_decisions_cov, little_test(X, alpha, "cov"))
+      
+    }
+    
+    little_power = c(little_power, mean(little_decisions))
+    little_power_cov = c(little_power_cov, mean(little_decisions_cov))
+  }
+
+plot(R, little_power, col="green", ylim = c(0,1), pch=18, xlab = "", ylab = "")
+points(R, little_power_cov, col="orange", pch=18)
+points(R, our_power, col="blue", pch=18)
+abline(h = alpha, col="red")
+legend("center",
+       legend = c("Little's power", "Little's power cov", "Our power"),
+       col = c("green", "orange", "blue"),
+       pch = c(18, 18, 18))
 
