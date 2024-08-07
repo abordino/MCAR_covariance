a21345699faa00502492c8fc3e2adec2d1437026
Alberto Bordino <54471782+abordino@users.noreply.github.com>
2024-08-07 09:30:30 +0100

    Update bootstrap_test.R

--- bootstrap_test.R
+++ bootstrap_test.R
@@ -1,4 +1,3 @@
-setwd("~/Documents/GitHub/MCAR")
 source("computeR.R")
 source("little_test.R")
 source("find_SigmaS.R")
@@ -13,8 +12,8 @@ MCAR_corr_test = function(X, alpha, B, type="np"){
   result = get_SigmaS(X)
   SigmaS = result$SigmaS; patterns = result$pattern; n_pattern = result$n_pattern; data_pattern = result$data_pattern
   
-  for (i in length(SigmaS)){
-    if (min(eigen(SigmaS[[i]])$values) < 10^-7){
+  for (i in 1:n_pattern){
+    if (det(as.matrix(SigmaS[[i]])) < 1e-10){
       print("SigmaS is singular!")
       return(NA)
     }
@@ -26,6 +25,9 @@ MCAR_corr_test = function(X, alpha, B, type="np"){
   QS_hat = list()
   for (i in 1:n_pattern){
     QS_hat[[i]] = Q_hat[patterns[[i]], patterns[[i]]]
+    if (det(as.matrix(QS_hat[[i]])) < 1e-10){
+      return(NA)
+    }
   }
   
   if (type=="np"){
@@ -97,8 +99,8 @@ MCAR_meancovTest = function(X, alpha, B, type="np"){
   muS = result$muS; C_S = result$C_S; sigma_squared_S = result$sigma_squared_S; SigmaS = result$SigmaS
   patterns = result$pattern; n_pattern = result$n_pattern; data_pattern = result$data_pattern
   
-  for (i in length(SigmaS)){
-    if (min(eigen(SigmaS[[i]])$values) < 10^-7){
+  for (i in 1:n_pattern){
+    if (det(as.matrix(C_S[[i]])) < 1e-10){
       print("SigmaS is singular!")
       return(NA)
     }
@@ -107,10 +109,12 @@ MCAR_meancovTest = function(X, alpha, B, type="np"){
   tmp = computeR(patterns, SigmaS)
   T_hat_0 = tmp$R + M(muS, patterns) + V(sigma_squared_S, patterns)
   Q_hat = tmp$Sigma/(1-tmp$R)
+  
   QS_hat = list()
   for (i in 1:n_pattern){
     QS_hat[[i]] = Q_hat[patterns[[i]], patterns[[i]]]
-    if (min(eigen(QS_hat[[i]])$values) < 10^-7){
+    if (det(as.matrix(QS_hat[[i]])) < 1e-10){
+      print("QS is singular!")
       return(NA)
     }
   }
@@ -132,7 +136,7 @@ MCAR_meancovTest = function(X, alpha, B, type="np"){
       for (i in 1:n_pattern){
         n_S = dim(rot_data_pattern[[i]])[1]
         tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
-        if(dim(unique(tmp_data))[1] < 2){
+        while(dim(unique(tmp_data))[1] < 2){
           tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
         }
         X[(1+r_ind):(n_S+r_ind), patterns[[i]]] = tmp_data
@@ -196,4 +200,84 @@ MCAR_meancovTest = function(X, alpha, B, type="np"){
   p_hat = (1+sum_indicator)/(B+1)
   decision = p_hat < alpha
   return(decision)
+}
+
+if (sys.nframe() == 0){
+  set.seed(7)
+  n = 200
+  MC = 30
+  d = 5
+  
+  # Select the copula
+  cp = claytonCopula(param = c(1), dim = d)
+  # Generate the multivariate distribution (in this case it is just bivariate) with normal and t marginals
+  P = mvdc(copula = cp, margins = c(rep("lnorm",d)),
+           paramMargins = rep(list(1),d) )
+  data = rMvdc(n, P)
+  
+  
+  #### generate dataset from patter S = {{1,2},{2,3},{1,3}}
+  X = delete_MCAR(data, .12, c(1,2))
+  result = get_SigmaS(X); d = result$ambient_dimension; av_sigma = compute_av("var", X)
+  
+  for (j in 1:d){
+    X[,j] = X[,j]/sqrt(av_sigma[j])
+  }
+  
+  result = get_SigmaS(X)
+  av_mu = compute_av("mean", X)
+  muS = result$muS; C_S = result$C_S; sigma_squared_S = result$sigma_squared_S; SigmaS = result$SigmaS
+  patterns = result$pattern; n_pattern = result$n_pattern; data_pattern = result$data_pattern
+  
+  for (i in length(SigmaS)){
+    if (min(eigen(SigmaS[[i]])$values) < 1e-10){
+      print("SigmaS is singular!")
+      return(NA)
+    }
+  }
+  
+  tmp = computeR(patterns, SigmaS)
+  T_hat_0 = tmp$R + M(muS, patterns) + V(sigma_squared_S, patterns)
+  Q_hat = tmp$Sigma/(1-tmp$R)
+  QS_hat = list()
+  for (i in 1:n_pattern){
+    QS_hat[[i]] = Q_hat[patterns[[i]], patterns[[i]]]
+  }
+  
+    #### rotate X, to make it look like it's from H0
+    rot_data_pattern = list()
+    for (i in 1:n_pattern){
+      rot_data_pattern[[i]] = t((sqrtm(as.matrix(QS_hat[[i]]))$B)%*%(sqrtm(as.matrix(C_S[[i]]))$Binv)%*%
+                                  t(data_pattern[[i]] - muS[[i]] +  av_mu[patterns[[i]]]))
+    }
+  
+    sum_indicator = 0
+      r_ind = 0
+      X = data.frame(matrix(nrow = d*n, ncol = d))
+      for (i in 1:n_pattern){
+        n_S = dim(rot_data_pattern[[i]])[1]
+        tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
+        if(dim(unique(tmp_data))[1] < 2){
+          tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
+        }
+        X[(1+r_ind):(n_S+r_ind), patterns[[i]]] = tmp_data
+        r_ind = r_ind + n_S
+      }
+      X = as.matrix(X[1:r_ind,])
+  
+      result = get_SigmaS(X); av_sigma = compute_av("var", X)
+  
+      for (j in 1:d){
+        X[,j] = X[,j]/sqrt(av_sigma[j])
+      }
+  
+      result = get_SigmaS(X)
+      patterns = result$pattern; n_pattern = result$n_pattern; data_pattern = result$data_pattern
+      muS_b = result$muS; C_S_b = result$C_S; sigma_squared_S_b = result$sigma_squared_S; SigmaS_b = result$SigmaS
+  
+      T_hat_b = computeR(patterns, SigmaS_b)$R + M(muS_b, patterns) + V(sigma_squared_S_b, patterns)
+  
+      if (T_hat_b >= T_hat_0){
+        sum_indicator = sum_indicator + 1
+      }
 }
