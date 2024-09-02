cd94f217c1293d9728c3991f97c35a8a89fcd120
Alberto Bordino <54471782+abordino@users.noreply.github.com>
2024-09-02 09:30:47 +0100

    Update computeR.R

--- computeR.R
+++ computeR.R
@@ -201,15 +201,612 @@ computeR = function(patterns=list(), SigmaS=list()) {
   #----------------------------------------------------------------------------------------
   #----------------------------------------------------------------------------------------
   #----------------------------------------------------------------------------------------
+  my_list = list("R" = R, "XS" = XS, "XS0" = XS0, "YS" = YS,
+                 "Sigma" = Sigma, "SigmaS" = SigmaS, 
+                 "SigmaSprime" = SigmaSprime)
+  return(my_list)
+  
+}
+
+computeRalpha = function(patterns=list(), SigmaS=list(), alpha) {
+  
+  #----------------------------------------------------------------------------------------
+  ##### DEFINE PATTERN IF NOT SPECIFIED
+  #----------------------------------------------------------------------------------------
+  add_pattern = TRUE
+  if (length(patterns) > 0){
+    add_pattern = FALSE
+  }
+  
+  i = 1
+  while(add_pattern == TRUE){
+    prompt1 = "Enter variable numbers (space-separated list): \n"
+    tmp = as.vector(as.integer(strsplit(readline(prompt1), " ")[[1]]))
+    patterns[[i]] = tmp
+    
+    prompt2 = "Do you want to add another pattern? (Write TRUE or FALSE): \n"
+    add_pattern = as.logical((readline(prompt2)))
+    i = i+1
+  }
+  
+  #----------------------------------------------------------------------------------------
+  # remove duplicates
+  patterns = unique(patterns)
+  card_patterns = length(patterns)
+  
+  d = 0 # computing d
+  for (S in patterns){
+    if (max(S) > d){
+      d = max(S)
+    }
+  }
+  
+  
+  
+  #----------------------------------------------------------------------------------------
+  ########## GENERATE SIGMA IF NOT SPECIFIED
+  #----------------------------------------------------------------------------------------
+  if (length(SigmaS) == 0){
+    prompt2 = "Do you want a random SigmaS or a compatible one? (Write 1 or 2 respectively): \n"
+    comp = as.numeric((readline(prompt2)))
+    
+    if (comp == 1){
+      for(j in 1:card_patterns){
+        card_S = length(patterns[[j]])
+        A = matrix(runif((card_S)^2)*2-1, ncol=card_S) 
+        tmp_0 = t(A) %*% A
+        SigmaS[[j]]=as.matrix(cov2cor(tmp_0))
+      }
+    } else if(comp == 2){
+      SigmaS=list()
+      A = matrix(runif(d^2)*2-1, ncol=d)
+      Sigma = cov2cor(t(A) %*% A)
+      for(j in 1:card_patterns){
+        SigmaS[[j]]=as.matrix(Sigma[patterns[[j]],patterns[[j]]])
+      }
+    } else {
+      return("An error occured.")
+    }
+  }
+  
+  #----------------------------------------------------------------------------------------
+  ########## WRITING THE SDP PROBLEM ##########################
+  #----------------------------------------------------------------------------------------
+  
+  # Matrix giving objective function (1/d)*tr(Sigma)
+  C = c(list((1/d)*diag(d)))
+  for(j in 1:card_patterns){
+    card_S = length(patterns[[j]])
+    C[[1+j]] = matrix(0, card_S, card_S)
+  }
+  C[[2+card_patterns]] = as.matrix(-alpha)
+  
+  # number of constraints
+  m = d-1
+  for(j in 1:card_patterns){
+    card_S = length(patterns[[j]])
+    m = m + card_S*(card_S+1)/2
+  }
+  
+  b=rep(0,m)
+  
+  A=rep(list(0),m)
+  Ablank=c(list(matrix(0,d,d)))
+  for(j in 1:card_patterns){
+    card_S = length(patterns[[j]])
+    Ablank[[1+j]] = matrix(0, card_S, card_S)
+  }
+  Ablank[[2+card_patterns]] = as.matrix(0)
+  
+  
+  ind = 0
+  for(k in 1:card_patterns){
+    card_S = length(patterns[[k]])
+    for (i in 1:card_S){
+      for (j in i:card_S){
+        b[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i] = SigmaS[[k]][i,j]
+        
+        e_i = rep(0, card_S); e_i[i]=1
+        e_j = rep(0, card_S); e_j[j]=1
+        A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]] = Ablank
+        A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]][[k+1]] = e_i%*%t(e_j)/2 + e_j%*%t(e_i)/2
+        A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]][[2+card_patterns]] = -1*as.matrix(as.numeric(i == j))
+        
+        tmp_1 = e_i%*%t(e_j)/2 +  e_j%*%t(e_i)/2
+        tmp_2 = matrix(0,d,d)
+        tmp_2[patterns[[k]],patterns[[k]]] = tmp_1
+        A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]][[1]] = tmp_2
+      }
+    }
+    ind = ind + card_S*(card_S+1)/2
+  }
+  
+  for (i in (d-2):0){
+    A[[m-i]] = Ablank
+    A[[m-i]][[1]][1,1]=1
+    A[[m-i]][[1]][2+i,2+i]=-1
+  }
+  
+  sizes = numeric(length = 2+card_patterns)
+  sizes[1] = d; ind = 2
+  for (S in patterns){
+    sizes[ind] = length(S)
+    ind = ind+1
+  }
+  sizes[2+card_patterns] = 1
+  
+  K = list(type=rep("s",2+card_patterns),size=sizes) # Variables are the PSD matrices Sigma,Z_{1,2},...,Z_{d,1}
+  
+  #----------------------------------------------------------------------------------------
+  echo=FALSE
+  SDP=csdp(C, A, b, K) #Running the function
+  R = 1-SDP$pobj
+  
+  
+  #----------------------------------------------------------------------------------------
+  # Optimal XS 
+  #----------------------------------------------------------------------------------------
+  tmp0=c(list())
+  for(j in 1:card_patterns){
+    card_S = length(patterns[[j]])
+    tmp0[[j]] = matrix(0, card_S, card_S)
+  }
+  
+  YS=tmp0
+  ind = 0
+  for (k in 1:card_patterns){
+    card_S = length(patterns[[k]])
+    for (i in 1:card_S){
+      for (j in i:card_S){
+        tmp = as.numeric(abs(SDP$y[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]/2) > 0.0001)*
+          SDP$y[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]
+        YS[[k]][i,j] = tmp/2
+        YS[[k]][j,i] = YS[[k]][j,i] + tmp/2
+        
+      }
+    }
+    ind = ind + card_S*(card_S+1)/2
+  }
+  
+  XS0 = tmp0
+  count = rep(0,d)
+  for (k in 1:card_patterns){
+    for(i in 1:d){
+      if(i %in% patterns[[k]]){
+        count[i] = count[i] + 1
+      }
+    }
+  }
+  
+  for (k in 1:card_patterns){
+    pattern = patterns[[k]]
+    if (length(pattern) == 1){
+      XS0[[k]] = as.matrix(1/count[pattern])
+    } else{
+      XS0[[k]] = diag(1/count[pattern])
+    }
+    
+  }
+  
+  XS = c(list())
+  for (k in 1:card_patterns){
+    XS[[k]] = d*YS[[k]] - XS0[[k]]
+  }
+  
+  #----------------------------------------------------------------------------------------
+  # Optimal Sigma
+  #----------------------------------------------------------------------------------------
+  Sigma = SDP$X[[1]]
+  
+  SigmaSprime = list()
+  for (i in 1:card_patterns){
+    SigmaSprime[[i]] = SDP$X[[i+1]]
+  }
+  
+  z = SDP$X[[2+card_patterns]]
+  
+  
+  #----------------------------------------------------------------------------------------
+  #----------------------------------------------------------------------------------------
+  #----------------------------------------------------------------------------------------
+  my_list = list("R" = R, "XS" = XS, "XS0" = XS0, "z" = z, "YS" = YS,
+                 "Sigma" = Sigma, "SigmaS" = SigmaS, 
+                 "SigmaSprime" = SigmaSprime)
+  return(my_list)
+  
+}
+
+computeRc = function(patterns=list(), SigmaS=list(), c) {
+  
+  #----------------------------------------------------------------------------------------
+  ##### DEFINE PATTERN IF NOT SPECIFIED
+  #----------------------------------------------------------------------------------------
+  add_pattern = TRUE
+  if (length(patterns) > 0){
+    add_pattern = FALSE
+  }
+  
+  i = 1
+  while(add_pattern == TRUE){
+    prompt1 = "Enter variable numbers (space-separated list): \n"
+    tmp = as.vector(as.integer(strsplit(readline(prompt1), " ")[[1]]))
+    patterns[[i]] = tmp
+    
+    prompt2 = "Do you want to add another pattern? (Write TRUE or FALSE): \n"
+    add_pattern = as.logical((readline(prompt2)))
+    i = i+1
+  }
+  
+  #----------------------------------------------------------------------------------------
+  # remove duplicates
+  patterns = unique(patterns)
+  card_patterns = length(patterns)
+  
+  d = 0 # computing d
+  for (S in patterns){
+    if (max(S) > d){
+      d = max(S)
+    }
+  }
+  
+  
+  
+  #----------------------------------------------------------------------------------------
+  ########## GENERATE SIGMA IF NOT SPECIFIED
+  #----------------------------------------------------------------------------------------
+  if (length(SigmaS) == 0){
+    prompt2 = "Do you want a random SigmaS or a compatible one? (Write 1 or 2 respectively): \n"
+    comp = as.numeric((readline(prompt2)))
+    
+    if (comp == 1){
+      for(j in 1:card_patterns){
+        card_S = length(patterns[[j]])
+        A = matrix(runif((card_S)^2)*2-1, ncol=card_S) 
+        tmp_0 = t(A) %*% A
+        SigmaS[[j]]=as.matrix(cov2cor(tmp_0))
+      }
+    } else if(comp == 2){
+      SigmaS=list()
+      A = matrix(runif(d^2)*2-1, ncol=d)
+      Sigma = cov2cor(t(A) %*% A)
+      for(j in 1:card_patterns){
+        SigmaS[[j]]=as.matrix(Sigma[patterns[[j]],patterns[[j]]])
+      }
+    } else {
+      return("An error occured.")
+    }
+  }
+  
+  #----------------------------------------------------------------------------------------
+  ########## WRITING THE SDP PROBLEM ##########################
+  #----------------------------------------------------------------------------------------
+  
+  # Matrix giving objective function (1/d)*tr(Sigma)
+  C = c(list((1/d)*diag(d)))
+  for(j in 1:card_patterns){
+    card_S = length(patterns[[j]])
+    C[[1+j]] = matrix(0, card_S, card_S)
+  }
+  for(j in 1:card_patterns){
+    card_S = length(patterns[[j]])
+    C[[card_patterns+1+j]] = matrix(0, card_S, card_S)
+  }
+  
+  # number of constraints
+  m = d-1
+  for(j in 1:card_patterns){
+    card_S = length(patterns[[j]])
+    m = m + 2*card_S*(card_S+1)/2
+  }
+  
+  b=rep(0,m)
+  
+  A=rep(list(0),m)
+  Ablank=c(list(matrix(0,d,d)))
+  for(j in 1:card_patterns){
+    card_S = length(patterns[[j]])
+    Ablank[[1+j]] = matrix(0, card_S, card_S)
+  }
+  for(j in 1:card_patterns){
+    card_S = length(patterns[[j]])
+    Ablank[[card_patterns+1+j]] = matrix(0, card_S, card_S)
+  }
+  
+  
+  ind = 0
+  for(k in 1:card_patterns){
+    card_S = length(patterns[[k]])
+    for (i in 1:card_S){
+      for (j in i:card_S){
+        b[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i] = SigmaS[[k]][i,j]
+        
+        e_i = rep(0, card_S); e_i[i]=1
+        e_j = rep(0, card_S); e_j[j]=1
+        A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]] = Ablank
+        A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]][[k+1]] = e_i%*%t(e_j)/2 + e_j%*%t(e_i)/2
+        
+        tmp_1 = e_i%*%t(e_j)/2 +  e_j%*%t(e_i)/2
+        tmp_2 = matrix(0,d,d)
+        tmp_2[patterns[[k]],patterns[[k]]] = tmp_1
+        A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]][[1]] = tmp_2
+      }
+    }
+    ind = ind + card_S*(card_S+1)/2
+  }
+  
+  ### new blocks
+  for(k in 1:card_patterns){
+    card_S = length(patterns[[k]])
+    for (i in 1:card_S){
+      for (j in i:card_S){
+        b[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i] = c*as.numeric(i == j)
+        
+        e_i = rep(0, card_S); e_i[i]=1
+        e_j = rep(0, card_S); e_j[j]=1
+        A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]] = Ablank
+        A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]][[card_patterns+k+1]] = 
+          -(e_i%*%t(e_j)/2 + e_j%*%t(e_i)/2)
+        
+        tmp_1 = e_i%*%t(e_j)/2 +  e_j%*%t(e_i)/2
+        tmp_2 = matrix(0,d,d)
+        tmp_2[patterns[[k]],patterns[[k]]] = tmp_1
+        A[[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]][[1]] = tmp_2
+      }
+    }
+    ind = ind + card_S*(card_S+1)/2
+  }
+  
+  for (i in (d-2):0){
+    A[[m-i]] = Ablank
+    A[[m-i]][[1]][1,1]=1
+    A[[m-i]][[1]][2+i,2+i]=-1
+  }
+  
+  sizes = numeric(length = 1+2*card_patterns)
+  sizes[1] = d; ind = 2
+  for (S in patterns){
+    sizes[ind] = length(S)
+    ind = ind+1
+  }
+  for (S in patterns){
+    sizes[ind] = length(S)
+    ind = ind+1
+  }
+  
+  K = list(type=rep("s",1+2*card_patterns),size=sizes) # Variables are the PSD matrices Sigma,Z_{1,2},...,Z_{d,1}
+  
+  #----------------------------------------------------------------------------------------
+  echo=FALSE
+  SDP=csdp(C, A, b, K) #Running the function
+  R = 1-SDP$pobj
+  
+  
+  #----------------------------------------------------------------------------------------
+  # Optimal XS 
+  #----------------------------------------------------------------------------------------
+  tmp0=c(list())
+  for(j in 1:card_patterns){
+    card_S = length(patterns[[j]])
+    tmp0[[j]] = matrix(0, card_S, card_S)
+  }
+  
+  YS=tmp0
+  ind = 0
+  for (k in 1:card_patterns){
+    card_S = length(patterns[[k]])
+    for (i in 1:card_S){
+      for (j in i:card_S){
+        tmp = as.numeric(abs(SDP$y[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]/2) > 0.0001)*
+          SDP$y[ind + (i-1)*(card_S+1)-(i-1)*i/2 + j+1-i]
+        YS[[k]][i,j] = tmp/2
+        YS[[k]][j,i] = YS[[k]][j,i] + tmp/2
+        
+      }
+    }
+    ind = ind + card_S*(card_S+1)/2
+  }
+  
+  XS0 = tmp0
+  count = rep(0,d)
+  for (k in 1:card_patterns){
+    for(i in 1:d){
+      if(i %in% patterns[[k]]){
+        count[i] = count[i] + 1
+      }
+    }
+  }
+  
+  for (k in 1:card_patterns){
+    pattern = patterns[[k]]
+    if (length(pattern) == 1){
+      XS0[[k]] = as.matrix(1/count[pattern])
+    } else{
+      XS0[[k]] = diag(1/count[pattern])
+    }
+    
+  }
+  
+  XS = c(list())
+  for (k in 1:card_patterns){
+    XS[[k]] = d*YS[[k]] - XS0[[k]]
+  }
+  
+  #----------------------------------------------------------------------------------------
+  # Optimal Sigma
+  #----------------------------------------------------------------------------------------
+  Sigma = SDP$X[[1]]
+  
+  SigmaSprime = list()
+  for (i in 1:card_patterns){
+    SigmaSprime[[i]] = SDP$X[[i+1]]
+  }
+  
+  
+  #----------------------------------------------------------------------------------------
+  #----------------------------------------------------------------------------------------
+  #----------------------------------------------------------------------------------------
   my_list = list("R" = R, "XS" = XS, "XS0" = XS0, "Sigma" = Sigma, "SigmaS" = SigmaS, 
                  "SigmaSprime" = SigmaSprime)
   return(my_list)
   
 }
 
+nuclearNorm = function(XS){
+  sum = 0 
+  for (i in 1:length(XS)){
+    sum = sum + sum(diag(XS[[i]]))
+  }
+  return(sum)
+}
+
+innerS = function(XS, YS){
+  sum = 0
+  for (i in 1:length(XS)){
+    sum = sum + sum(diag(XS[[i]]%*%YS[[i]]))
+  }
+  return(sum)
+}
+
 if (sys.nframe() == 0){
   
-  ####################### EXAMPLES TO TEST THE FUNCTION ###########################################
+  
+
+    ####################### EXAMPLES TO TEST THE FUNCTION ###########################################
+X = c()
+Y = c()
+for (xxx in 1:10000){
+  d = 3
+  SigmaS=list() #Random 2x2 correlation matrices (necessarily consistent)
+  for(j in 1:d){
+    x=runif(2,min=-1,max=1); y=runif(2,min=-1,max=1); SigmaS[[j]]=cov2cor(x%*%t(x) + y%*%t(y))
+  }
+  SigmaS
+  
+  tmp = computeR(list(c(1,2),c(2,3), c(1,3)), SigmaS = SigmaS)
+  tmp$R
+  
+  theta_1 = acos(SigmaS[[1]][1,2])
+  theta_2 = acos(SigmaS[[2]][1,2])
+  theta_3 = acos(SigmaS[[3]][1,2])
+  
+  while(theta_2 > pi/2 || theta_3 > pi/2 || theta_1 < max(theta_2, theta_3)){
+    d = 3
+    SigmaS=list() #Random 2x2 correlation matrices (necessarily consistent)
+    for(j in 1:d){
+      x=runif(2,min=-1,max=1); y=runif(2,min=-1,max=1); SigmaS[[j]]=cov2cor(x%*%t(x) + y%*%t(y))
+    }
+    SigmaS
+    
+    tmp = computeR(list(c(1,2),c(2,3), c(1,3)), SigmaS = SigmaS)
+    tmp$R
+    
+    theta_1 = acos(SigmaS[[1]][1,2])
+    theta_2 = acos(SigmaS[[2]][1,2])
+    theta_3 = acos(SigmaS[[3]][1,2])
+    
+  }
+  
+  phi_1 = acos(tmp$Sigma[1,2]/(1-tmp$R))
+  # (cos(phi_1) - cos(theta_1))/(1 + cos(phi_1))
+  phi_2 = acos(tmp$Sigma[2,3]/(1-tmp$R))
+  phi_3 = acos(tmp$Sigma[1,3]/(1-tmp$R))
+  
+  # 2*sin(0.5*(phi_1+theta_1))*sin(0.5*(theta_1 - theta_2 - theta_3))/(1 + cos(phi_1))
+  beta = phi_2 + phi_3 - theta_2 - theta_3
+  Y = c(Y, beta)
+  X = c(X, tmp$R)
+}
+
+plot(X,Y, type="p")
+  
+  
+    
+    # SigmaS[[3]][1,2] = cos(0)
+    # SigmaS[[3]][2,1] = cos(0)
+    # SigmaS[[1]][1,2] = cos(0)
+    # SigmaS[[1]][2,1] = cos(0)
+    # SigmaS[[2]][1,2] = 0.90
+    # SigmaS[[2]][2,1] = 0.90
+    
+    
+    
+    B = matrix(c(1, SigmaS[[1]][1,2], SigmaS[[3]][1,2],
+                 SigmaS[[1]][1,2], 1, SigmaS[[2]][1,2], 
+                 SigmaS[[3]][1,2], SigmaS[[2]][1,2], 1), nrow = 3)
+    c = min(eigen(B)$values)
+    
+    beta = 1000
+    old = computeR(list(c(1,2),c(2,3), c(1,3)), SigmaS = SigmaS)$R
+    tmp = computeRalpha(list(c(1,2),c(2,3), c(1,3)), SigmaS = SigmaS, beta)
+    neww = tmp$R
+    z = tmp$z
+    
+    old
+    neww
+    z
+    c
+    
+    SigmaS[[1]]+as.numeric(z)*diag(2)
+    tmp$SigmaSprime[[1]]+tmp$Sigma[c(1,2),c(1,2)]
+  
+  
+  
+  
+  ####### other
+  d = 5
+  SigmaS=list() 
+  for(j in 1:d){
+    A = matrix(runif((d-1)^2)*2-1, ncol=d-1) 
+    tmp_0 = t(A) %*% A
+    SigmaS[[j]]=as.matrix(cov2cor(tmp_0))
+  }
+  
+  c = 100
+  for(i in 1:d){
+    cand = min(eigen(SigmaS[[i]])$values)
+    if (cand < c){
+      c = cand
+    }
+  }
+  c
+  
+  old = computeR(patterns = list(c(2,3,4,5),c(1,3,4,5),c(1,2,4,5),c(1,2,3,5),c(1,2,3,4)), SigmaS = SigmaS)$R
+  new = computeRc(patterns = list(c(2,3,4,5),c(1,3,4,5),c(1,2,4,5),c(1,2,3,5),c(1,2,3,4)), SigmaS = SigmaS, 0.1)$R
+  neww = computeRalpha(patterns = list(c(2,3,4,5),c(1,3,4,5),c(1,2,4,5),c(1,2,3,5),c(1,2,3,4)), SigmaS = SigmaS, 55)$R
+  z = computeRalpha(patterns = list(c(2,3,4,5),c(1,3,4,5),c(1,2,4,5),c(1,2,3,5),c(1,2,3,4)), SigmaS = SigmaS, 55)$z
+  
+  new
+  old
+  neww
+  
+  z
+  
+  
+  
+  c
+  old+c
+  
+  
+### Miss methods
+  d = 3
+  n = 800
+  cp = claytonCopula(param = c(1), dim = d)
+  #----------------------------------------------------------------------------------------
+  # Generate the multivariate distribution
+  #----------------------------------------------------------------------------------------
+  P = mvdc(copula = cp, margins = c(rep(yyy,d)),
+           paramMargins = rep(list(rate = 1),d)) #list(c(mean = 0, sd = 1))
+  
+  data = rMvdc(n, P)
+  X = delete_MAR_rank(data, 0.3, c(1,2), cols_ctrl = c(3, 3))
+  result = get_SigmaS(X)
+  
+  old = computeR(result$patterns, result$SigmaS)$R
+  neww = computeRalpha(result$patterns, result$SigmaS, 10)$R
+  
+  old
+  neww
+  
   ############# test on 3-cycle ####################
   R_rho = c()
   d = 3
@@ -477,4 +1074,3 @@ if (sys.nframe() == 0){
     curve(4*x^1, add=T, col="darkgreen")
   } 
 }
-
