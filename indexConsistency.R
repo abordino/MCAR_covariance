f2682f20cbe616bd4070ed2fc633dbd6c0ab6a40
Alberto Bordino <54471782+abordino@users.noreply.github.com>
2024-09-09 11:25:45 +0100

    Update indexConsistency.R

--- indexConsistency.R
+++ indexConsistency.R
@@ -2,15 +2,6 @@ source("find_SigmaS.R")
 library(missMethods)
 library(MASS)
 library(norm)
-library(foreach)
-library(doSNOW)
-library(doParallel)
-library(future)
-library(parallel)
-library(foreach)
-library(doRNG) 
-library(doFuture)
-library(future.apply)
 
 #### compute av_\mu and av_\sigma
 av_sigma = function(value_S, patterns){
@@ -71,45 +62,6 @@ M = function(mu_S, patterns, MahS, n_S){
 
   return(sum/sum(unlist(n_S)))
 }
-
-# V = function(sigma_squared_S, patterns, MahS, n_S){
-#   
-#   n_pattern = length(patterns)
-#   d = max(unlist(patterns))
-#   av = rep(1, d)
-#   
-#   sum = 0
-#   for(i in 1:n_pattern){
-#     # sum = sum + sum(abs(mu_S[[i]] - av_mu[patterns[[i]]]))
-#     sum = sum + n_S[[i]]*(sigma_squared_S[[i]] - av[patterns[[i]]])%*%solve(MahS[[i]])%*%
-#       (sigma_squared_S[[i]] - av[patterns[[i]]])
-#   }
-#   
-#   return(sum/sum(unlist(n_S)))
-# }
-
-
-# M = function(mu_S, patterns, MahS, n_S){
-# 
-#   av_mu = av_mu(mu_S, patterns, n_S)
-#   n_pattern = length(patterns)
-#   d = max(unlist(patterns))
-# 
-#   sum = 0
-#   max = 0
-#   for(i in 1:n_pattern){
-# 
-#     candidate = (mu_S[[i]] - av_mu[patterns[[i]]])%*%solve(MahS[[i]])%*%
-#       (mu_S[[i]] - av_mu[patterns[[i]]])
-#     if (candidate > max){
-#       max = candidate
-#     }
-#     sum = sum + candidate
-#   }
-# 
-#   return(sum/(n_pattern*max))
-# }
-
 
 V = function(sigma_squared_S, patterns){
 
