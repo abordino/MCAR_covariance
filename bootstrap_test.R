0a15ef0b235b3bdfc5db634f061cc216e65b7d14
Alberto Bordino <54471782+abordino@users.noreply.github.com>
2024-10-09 11:11:42 +0100

    Update bootstrap_test.R

--- bootstrap_test.R
+++ bootstrap_test.R
@@ -1,5 +1,4 @@
 source("computeR.R")
-source("little_test.R")
 source("find_SigmaS.R")
 source("indexConsistency.R")
 library(missMethods)
@@ -21,27 +20,21 @@ mySqrtm = function(A){
   return(V %*% abs(D)^(1/2) %*% U)
 }
 
-MCAR_meancovTest = function(X, alpha, B){
+corr.compTest = function(X, B){
   
-  #----------------------------------------------------------------------------------------
-  ## rescale the data
-  #----------------------------------------------------------------------------------------
+  ####--------------------------------------------------------------------------
+  #### compute R^0
+  ####--------------------------------------------------------------------------
+  
   result = get_SigmaS(X)
-  d = result$ambient_dimension;  N_S = result$n_S
+  d = result$ambient_dimension; N_S = result$n_S; n = sum(unlist(N_S))
   n_pattern = result$n_pattern; patterns = result$pattern
-  av_sigma = av_sigma(result$sigma_squared_S, patterns)
   
-  for (j in 1:d){
-    X[,j] = X[,j]/sqrt(av_sigma[j])
-  }
+  SigmaS = result$SigmaS 
   
-  #----------------------------------------------------------------------------------------
-  ## compute means and covariance; compute T0
-  #----------------------------------------------------------------------------------------
-  result = get_SigmaS(X)
-  mu_S = result$mu_S; av_mu = av_mu(mu_S, patterns, N_S)
-  C_S = result$C_S; sigma_squared_S = result$sigma_squared_S; SigmaS = result$SigmaS
   data_pattern = result$data_pattern
+  
+  ####--------------------------------------------------------------------------
   
   tmp = computeR(patterns, SigmaS)
   Q_hat = tmp$Sigma/(1-tmp$R)
@@ -51,65 +44,199 @@ MCAR_meancovTest = function(X, alpha, B){
     QS_hat[[i]] = Q_hat[patterns[[i]], patterns[[i]]]
   }
   
-  T_hat_0 = tmp$R + V(sigma_squared_S, patterns) + M(mu_S, patterns, C_S, N_S) 
+  R_hat_0 = tmp$R
   
-  #----------------------------------------------------------------------------------------
+  ####--------------------------------------------------------------------------
   #### rotate X, to make it look like it's from H0
-  #----------------------------------------------------------------------------------------
-    rot_data_pattern = list()
+  ####--------------------------------------------------------------------------
+  rot_data_pattern = list()
+  for (i in 1:n_pattern){
+    rot_data_pattern[[i]] = scale(data_pattern[[i]]) %*%
+      solve(mySqrtm(as.matrix(SigmaS[[i]]))) %*%
+      mySqrtm(as.matrix(QS_hat[[i]]))
+  }
+  
+  sum_indicator = 0
+  for (b in 1:B){
+    r_ind = 0
+    X = data.frame(matrix(nrow = d*n, ncol = d))
     for (i in 1:n_pattern){
-      rot_data_pattern[[i]] = t((mySqrtm(as.matrix(QS_hat[[i]])))%*%
-                                  (solve(mySqrtm(as.matrix(C_S[[i]]))))%*%
-                                  t(data_pattern[[i]] - mu_S[[i]] +  av_mu[patterns[[i]]]))
-    }
-    
-    sum_indicator = 0
-    for (b in 1:B){
-      r_ind = 0
-      X = data.frame(matrix(nrow = d*n, ncol = d))
-      for (i in 1:n_pattern){
-        n_S = dim(rot_data_pattern[[i]])[1]
+      n_S = dim(rot_data_pattern[[i]])[1]
+      tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
+      while(dim(unique(tmp_data))[1] <= dim(tmp_data)[2]){
         tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
-        while(dim(unique(tmp_data))[1] <= dim(tmp_data)[2]){
-          tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
-        }
-        X[(1+r_ind):(n_S+r_ind), patterns[[i]]] = tmp_data
-        r_ind = r_ind + n_S
       }
-      X = as.matrix(X[1:r_ind,])
-        
-      #----------------------------------------------------------------------------------------
-      ## rescale the rotated data
-      #----------------------------------------------------------------------------------------
-      result = get_SigmaS(X)
-      av_sigma = av_sigma(result$sigma_squared_S, result$patterns)
-        
-      for (j in 1:d){
-        X[,j] = X[,j]/sqrt(av_sigma[j])
-      }
-      
-      #----------------------------------------------------------------------------------------
-      ## compute Tb
-      #----------------------------------------------------------------------------------------
-      result = get_SigmaS(X)
-      mu_S_b = result$mu_S
-      SigmaS_b = result$SigmaS; C_S_b = result$C_S; sigma_squared_S_b = result$sigma_squared_S
-      patterns = result$pattern; n_pattern = result$n_pattern; data_pattern = result$data_pattern
-    
-      tmp = computeR(patterns, SigmaS_b)
-      
-      T_hat_b = tmp$R + V(sigma_squared_S_b, patterns) +
-        M(mu_S_b, patterns, C_S_b, N_S)
-      
-      #----------------------------------------------------------------------------------------
-      ## generate p-value
-      #----------------------------------------------------------------------------------------
-      if (T_hat_b >= T_hat_0){
-        sum_indicator = sum_indicator + 1
-      }
+      X[(1+r_ind):(n_S+r_ind), patterns[[i]]] = tmp_data
+      r_ind = r_ind + n_S
     }
+    X = as.matrix(X[1:r_ind,])
+    
+    ####--------------------------------------------------------------------------
+    #### compute R^b
+    ####--------------------------------------------------------------------------
+    result = get_SigmaS(X)
+    SigmaS_b = result$SigmaS
+    
+    R_hat_b = computeR(patterns, SigmaS_b)$R
+    
+    if (R_hat_b >= R_hat_0){
+      sum_indicator = sum_indicator + 1
+    }
+  }
   
   p_hat = (1+sum_indicator)/(B+1)
-  decision = p_hat < alpha
-  return(decision)
+  return(p_hat)
+  
+}
+
+mean.consTest = function(X, B){
+  
+  ####--------------------------------------------------------------------------
+  #### compute M^0
+  ####--------------------------------------------------------------------------
+  
+  result = get_SigmaS(X)
+  d = result$ambient_dimension; N_S = result$n_S; n = sum(unlist(N_S))
+  n_pattern = result$n_pattern; patterns = result$pattern
+  
+  mu_S = result$mu_S
+  
+  data_pattern = result$data_pattern
+  
+  ####--------------------------------------------------------------------------
+  
+  M_hat_0 = M(mu_S, patterns) 
+  
+  ####--------------------------------------------------------------------------
+  #### rotate X, to make it look like it's from H0
+  ####--------------------------------------------------------------------------
+  
+  # avmu = av(mu_S, patterns)
+  s = prelim.norm(as.matrix(X))
+  thetahat = em.norm(s)
+  avmu = getparam.norm(s,thetahat,corr=FALSE)$mu
+  
+  rot_data_pattern = list()
+  for (i in 1:n_pattern){
+    rot_data_pattern[[i]] = data_pattern[[i]] - 
+      do.call(rbind, replicate(dim(data_pattern[[i]])[1], mu_S[[i]], simplify = FALSE)) +
+      do.call(rbind, replicate(dim(data_pattern[[i]])[1], avmu[patterns[[i]]], simplify = FALSE))
+  }
+  
+  sum_indicator = 0
+  for (b in 1:B){
+    
+    r_ind = 0
+    X = data.frame(matrix(nrow = d*n, ncol = d))
+    for (i in 1:n_pattern){
+      n_S = dim(rot_data_pattern[[i]])[1]
+      tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
+      while(dim(unique(tmp_data))[1] <= dim(tmp_data)[2]){
+        tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
+      }
+      X[(1+r_ind):(n_S+r_ind), patterns[[i]]] = tmp_data
+      r_ind = r_ind + n_S
+    }
+    
+    X = as.matrix(X[1:r_ind,])
+    
+    ####--------------------------------------------------------------------------
+    #### compute M^b
+    ####--------------------------------------------------------------------------
+    
+    result = get_SigmaS(X)
+    mu_S_b = result$mu_S
+    
+    M_hat_b = M(mu_S_b, patterns)
+    
+    if (M_hat_b >= M_hat_0){
+      sum_indicator = sum_indicator + 1
+    }
+  }
+  
+  p_hat = (1+sum_indicator)/(B+1)
+  return(p_hat)
+}
+
+var.consTest = function(X, B){
+  
+  ####--------------------------------------------------------------------------
+  #### rescale the data
+  ####--------------------------------------------------------------------------
+  
+  result = get_SigmaS(X)
+  d = result$ambient_dimension; N_S = result$n_S; n = sum(unlist(N_S))
+  n_pattern = result$n_pattern; patterns = result$pattern
+  avsigma = av(result$sigma_squared_S, patterns)
+  
+  for (j in 1:d){
+    X[,j] = X[,j]/sqrt(avsigma[j])
+  }
+  
+  ####--------------------------------------------------------------------------
+  #### compute V^0
+  ####--------------------------------------------------------------------------
+  result = get_SigmaS(X)
+  sigma_squared_S = result$sigma_squared_S
+  
+  data_pattern = result$data_pattern
+  
+  V_hat_0 = V(sigma_squared_S, patterns) 
+  
+  ####--------------------------------------------------------------------------
+  #### rotate X, to make it look like it's from H0
+  ####--------------------------------------------------------------------------
+  
+  avsigma = av(sigma_squared_S, patterns)
+  # s = prelim.norm(as.matrix(X))
+  # thetahat = em.norm(s)
+  # avsigma = diag(getparam.norm(s,thetahat,corr=FALSE)$sigma)
+  
+  rot_data_pattern = list()
+  for (i in 1:n_pattern){
+    rot_data_pattern[[i]] = data_pattern[[i]]%*%
+      diag(1/sqrt(sigma_squared_S[[i]]), ncol = length(sigma_squared_S[[i]]))%*% 
+      diag(sqrt(avsigma[patterns[[i]]]), ncol = length(avsigma[patterns[[i]]]))
+  }
+  
+  sum_indicator = 0
+  for (b in 1:B){
+    
+    r_ind = 0
+    X = data.frame(matrix(nrow = d*n, ncol = d))
+    for (i in 1:n_pattern){
+      n_S = dim(rot_data_pattern[[i]])[1]
+      tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
+      while(dim(unique(tmp_data))[1] <= dim(tmp_data)[2]){
+        tmp_data = as.matrix(rot_data_pattern[[i]][sample(1:n_S, n_S, replace = T),])
+      }
+      X[(1+r_ind):(n_S+r_ind), patterns[[i]]] = tmp_data
+      r_ind = r_ind + n_S
+    }
+    
+    X = as.matrix(X[1:r_ind,])
+    
+    ####--------------------------------------------------------------------------
+    #### rescale data and compute V^b
+    ####--------------------------------------------------------------------------
+    
+    result = get_SigmaS(X)
+    avsigma = av(result$sigma_squared_S, result$patterns)
+    
+    for (j in 1:d){
+      X[,j] = X[,j]/sqrt(avsigma[j])
+    }
+    
+    result = get_SigmaS(X)
+    sigma_squared_S_b = result$sigma_squared_S
+    
+    V_hat_b = V(sigma_squared_S_b, patterns)
+    
+    if (V_hat_b >= V_hat_0){
+      sum_indicator = sum_indicator + 1
+    }
+  }
+  
+  p_hat = (1+sum_indicator)/(B+1)
+  return(p_hat)
 }
