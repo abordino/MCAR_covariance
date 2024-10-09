8853b9b3de9904482b8070817541c2d0c6491ff3
Alberto Bordino <54471782+abordino@users.noreply.github.com>
2024-10-09 11:13:15 +0100

    Update indexConsistency.R

--- indexConsistency.R
+++ indexConsistency.R
@@ -2,31 +2,9 @@ source("find_SigmaS.R")
 library(missMethods)
 library(MASS)
 library(norm)
+library(pracma)
 
-#### compute av_\mu and av_\sigma
-av_sigma = function(value_S, patterns){
-  
-  n_pattern = length(patterns)
-  d = max(unlist(patterns))
-  
-  res = numeric(length = d)
-  for (j in 1:d){
-    tmp = 0
-    card_Sj = 0
-    for (i in 1:n_pattern){
-      if (j %in% patterns[[i]]){
-        tmp = tmp + value_S[[i]][which(patterns[[i]] == j)[[1]]]
-        card_Sj = card_Sj+1
-      }
-    }
-    
-    res[j] = tmp/card_Sj
-  }
-  
-  return(res)
-}
-
-av_mu = function(mu_S, patterns, n_S){
+av = function(x_S, patterns){
   n_pattern = length(patterns)
   d = max(unlist(patterns))
   
@@ -36,8 +14,8 @@ av_mu = function(mu_S, patterns, n_S){
     den = 0
     for (i in 1:n_pattern){
       if (j %in% patterns[[i]]){
-        num = num + n_S[[i]]*mu_S[[i]][which(patterns[[i]] == j)[[1]]]
-        den = den + n_S[[i]]
+        num = num + x_S[[i]][which(patterns[[i]] == j)[[1]]]
+        den = den + 1
       }
     }
     
@@ -47,29 +25,38 @@ av_mu = function(mu_S, patterns, n_S){
   return(res)
 }
 
-M = function(mu_S, patterns, MahS, n_S){
-
-  av_mu = av_mu(mu_S, patterns, n_S)
+M = function(x_S, patterns){
+  
+  av_x = av(x_S, patterns)
   n_pattern = length(patterns)
   d = max(unlist(patterns))
-
-  sum = 0
+  
+  num = 0
   for(i in 1:n_pattern){
-    dim_S = length(patterns[[i]])
-    MahS[[i]] = diag(dim_S)
-    sum = sum + n_S[[i]]*sum(abs(mu_S[[i]] - av_mu[patterns[[i]]]))
+    cand = Norm(x_S[[i]] - av_x[patterns[[i]]], p = 1)
+    if (cand > num){
+      num = cand
+    }
+  } 
+  
+  den = 0
+  for(i in 1:n_pattern){
+    cand = length(patterns[[i]])
+    if (cand > den){
+      den = cand
+    }
   }
-
-  return(sum/sum(unlist(n_S)))
+  
+  return(num/den)
 }
 
 V = function(sigma_squared_S, patterns){
-
+  
   n_pattern = length(patterns)
   d = max(unlist(patterns))
-
+  
   min = 1
-
+  
   for (j in 1:d){
     for (i in 1:n_pattern){
       if (j %in% patterns[[i]]) {
@@ -80,6 +67,6 @@ V = function(sigma_squared_S, patterns){
       }
     }
   }
-
+  
   return(1-min)
 }
